3c8d1cd8a89564aa2ae6901e17911348772cd75f
rajat4999 <rajat.20243229@mnnit.ac.in>
2026-01-08 11:21:04 +0530

    complaint schema added

---
+++ backend/models/complaint.js
@@ -0,0 +1,45 @@
+const mongooose=require('mongoose');
+const complaintSchema=new mongoose.Schema({
+  student:{
+    type:mongooose.Schema.Types.ObjectId,
+    ref:'student',
+    required:true
+  },
+  category:{
+    type:String,
+    enum:['electrical','plumbing','cleaning','furniture','other'],
+    required:true
+  },
+  description:{
+    type:String,
+    required: true,
+  },
+  isCommon:{
+    type:Boolean,
+    default:false,
+    required:true
+  },
+  location:{
+    type:String,
+    trim:true,
+    required:()=> {return this.isCommon==true}
+  },
+  date:{
+    type:Date,
+    default:Date.now
+  },
+  status:{
+    type:String,
+    enum:['pending','worker assigned','resolved'],
+    default:'pending'
+  },
+  resolvedOn:{
+    type:Date
+  },
+  image:{
+    type:String  //url of image
+  }
+});
+
+const complaint=mongoose.model('complaint',complaintSchema);
+module.exports=complaint;
