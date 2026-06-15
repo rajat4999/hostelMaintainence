2862410fb5d15e14199dfe8eb81460008b52a7ce
rajat4999 <rajat.20243229@mnnit.ac.in>
2026-06-15 21:28:07 +0530

    final commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a central "caller"
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Your Backend URL
+  baseURL: import.meta.env.VITE_API_URL, // Your Backend URL
 });
 
 // Automatically add the Token to every request if it exists
